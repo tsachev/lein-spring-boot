a015af53d303e2547962a585173d32fdfe6631d4
Vladimir Tsanev <tsachev@gmail.com>
2018-06-13 22:48:02 +0300

    Upgrade spring-boot version

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://github.com/tsachev/lein-spring-boot"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.springframework.boot/spring-boot-loader-tools "1.5.8.RELEASE"]
+  :dependencies [[org.springframework.boot/spring-boot-loader-tools "2.0.2.RELEASE"]
                  [clojurewerkz/propertied "1.3.0"]]
-  :min-lein-version "2.7.0"
+  :profiles {:dev {:dependencies [[org.springframework.boot/spring-boot-maven-plugin "2.0.2.RELEASE"]]}}
+  :min-lein-version "2.8.1"
   :eval-in-leiningen true)
--- src/leiningen/spring_boot/build_info.clj
+++ src/leiningen/spring_boot/build_info.clj
@@ -1,7 +1,8 @@
 (ns leiningen.spring-boot.build-info
   (:require [clojure.java.io :as io])
   (:import (org.springframework.boot.loader.tools BuildPropertiesWriter
-                                                  BuildPropertiesWriter$ProjectDetails)))
+                                                  BuildPropertiesWriter$ProjectDetails)
+           (java.time Instant)))
 
 (defn build-info
   "Generate a build-info.properties file for the current project"
@@ -9,4 +10,9 @@
   (-> (BuildPropertiesWriter.
         (io/file (:compile-path project) "META-INF/build-info.properties"))
       (.writeBuildProperties
-        (BuildPropertiesWriter$ProjectDetails. (:group project) (:name project) (:version project) (:name project) {}))))
+        (BuildPropertiesWriter$ProjectDetails. (:group project)
+                                               (:name project)
+                                               (:version project)
+                                               (:name project)
+                                               (Instant/now)
+                                               {}))))
